c561b2341d23ec760a442048603ed33f45eb7e19
Miles Zimmerman <miles@maztek.io>
2017-02-07 15:24:56 -0800

    Add scaffold

---
+++ ByomeConnect.cs
@@ -0,0 +1,13 @@
+using System;
+
+using Oxide.Core;
+using Oxide.Core.Plugins;
+
+namespace OxidePlugins {
+  [Info("ByomeConnect", "mileszim", "0.0.1")]
+  [Description("Send and receive information to byome.io")]
+
+  class ByomeConnect : RustPlugin {
+
+  }
+}
