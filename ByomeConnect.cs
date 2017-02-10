1ae88f4b346ed30cd28d8790e7581854a5e5cc03
Miles Zimmerman <miles@maztek.io>
2017-02-10 05:10:17 -0800

    Player connect and disconnect queue

--- ByomeConnect.cs
+++ ByomeConnect.cs
@@ -1,13 +1,86 @@
 using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
 
 using Oxide.Core;
 using Oxide.Core.Plugins;
+using Oxide.Game.Rust;
+using Oxide.Game.Rust.Libraries.Covalence;
 
-namespace OxidePlugins {
-  [Info("ByomeConnect", "mileszim", "0.0.1")]
+namespace Oxide.Plugins {
+  [Info("ByomeConnect", "byome, inc", "0.0.1")]
   [Description("Send and receive information to byome.io")]
 
   class ByomeConnect : RustPlugin {
 
+    /**
+     * Plugin Config Stuff
+     */
+    protected override void LoadDefaultConfig() {
+      PrintWarning("Creating default ByomeConnect config file.");
+      Config.Set("databaseURL", "https://YOUR-FIREBASE-APP.firebaseio.com");
+      Config.Set("apiKey", "your_api_key_here");
+      Config.Set("serverId", "server_id_here");
+      SaveConfig();
+    }
+
+    void Init() {
+
+    }
+
+
+
+    /**
+     * Firebase requests and connection methods
+     */
+    private string serverEndpoint() {
+      return "servers" + Config["serverId"] + "/";
+    }
+
+    private string requestEndpoint(string path) {
+      return Config["databaseURL"] + "/" + path + ".json?auth=" + Config["apiKey"];
+    }
+
+    private void postRequest(string path, string body) {
+      webrequest.EnqueuePost(requestEndpoint(path), body, (code, response) => postRequestCallback(code, response), this);
+    }
+
+    private void postRequestCallback(int code, string response) {
+      if (response == null || code != 200) {
+        Puts($"Error: {code} - {response}");
+        return;
+      }
+    }
+
+
+
+    /**
+     * Server Hooks
+     */
+    void OnServerInitialized() {
+
+    }
+
+    void OnPlayerInit(BasePlayer player) {
+      var playerObject = new Dictionary<string, string> {
+        { "event", "player_connected" },
+        { "server", Convert.ToString(Config.Get("serverId")) },
+        { "id", player.UserIDString },
+        { "name", player.displayName },
+        { "ipAddress", Convert.ToString(player.net.connection.ipaddress) }
+      };
+      postRequest("queue/tasks", JsonConvert.SerializeObject(playerObject));
+    }
+
+    void OnPlayerDisconnected(BasePlayer player, string reason) {
+      var playerObject = new Dictionary<string, string> {
+        { "event", "player_disconnected" },
+        { "server", Convert.ToString(Config.Get("serverId")) },
+        { "id", player.UserIDString },
+        { "reason", reason },
+      };
+      postRequest("queue/tasks", JsonConvert.SerializeObject(playerObject));
+    }
   }
 }
